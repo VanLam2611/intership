c6cc33509cc1a8084be48006a26379898a67c245
Van Lam <levanlam3447@gmail.com>
2021-07-23 17:03:14 +0700

    update schema

--- config/lighthouse.php
+++ config/lighthouse.php
@@ -29,12 +29,12 @@ return [
          * make sure to return spec-compliant responses in case an error is thrown.
          */
         'middleware' => [
-            \Nuwave\Lighthouse\Support\Http\Middleware\AcceptJson::class,
+            //\Nuwave\Lighthouse\Support\Http\Middleware\AcceptJson::class,
 
             // Logs in a user if they are authenticated. In contrast to Laravel's 'auth'
             // middleware, this delegates auth and permission checks to the field level.
-            \Nuwave\Lighthouse\Support\Http\Middleware\AttemptAuthentication::class,
-
+            //\Nuwave\Lighthouse\Support\Http\Middleware\AttemptAuthentication::class,
+            ['auth:api'],
             // Logs every incoming GraphQL query.
             // \Nuwave\Lighthouse\Support\Http\Middleware\LogGraphQLQueries::class,
         ],
